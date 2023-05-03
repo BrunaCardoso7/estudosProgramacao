2538826856f87bb31dd69605793bd9890a4a5b4b
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-03 11:37:43 -0300

    Update exc10Array.js

--- exc10Array.js
+++ exc10Array.js
@@ -1,14 +1,21 @@
 const filmes = [
     {filme: 'Moana', ano:2023},
-    {filme: 'Pocoio', ano:2021},
+    {filme: 'Avatar', ano:2021},
     {filme: 'kimetsu no yaiba', ano:2022}
 ]
-
+let prim_letra = []
 const verificador = filmes.reduce((count, filmeAtual)=>{
-    let firstletter =  filmeAtual.filme[0]
-    console.log(firstletter)
+    
+    let firstletter =  filmeAtual.filme
+    prim_letra.push(firstletter)
+
+    return prim_letra
 }, 0)
- 
+let organizador = prim_letra.sort()
+for (c of organizador){
+    return c
+}
+
 
 
 
