9e2d6f8e8d536ffcbc318616db9d45755aeff945
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-12 22:52:53 -0300

    Update exc44POO.js

--- exc44POO.js
+++ exc44POO.js
@@ -1,1 +1,26 @@
-// 
+//Crie uma classe de um sistema de gerenciamento de estoque com métodos para adicionar, remover e buscar produtos. 
+// Crie também atributos para armazenar o nome, a descrição, o preço e a quantidade em estoque de cada produto.
+class estoque{
+    constructor(){
+         this.produto = []
+    }
+    adicionar(nome, preco, quantidade){
+        let listaProduto = {}
+        listaProduto.nome = nome
+        listaProduto.preco = preco
+        listaProduto.estoque = quantidade
+
+        return this.produto.push(listaProduto)
+    }
+    remover(){
+
+    }
+    buscar(){
+
+    }
+}
+let leite = new estoque()
+leite.adicionar("leite", 10.00, 20)
+for(c of this.produto){
+    console.log(c)
+}
