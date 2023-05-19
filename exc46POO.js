ca7cad8322e7658164ab66cb813c00008d68777c
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-18 21:39:32 -0300

    atualização

---
+++ exc46POO.js
@@ -0,0 +1,11 @@
+// Crie uma classe Produto que represente um produto com os atributos nome, preço e quantidade em estoque. 
+// Em seguida, crie um array de objetos Produto e implemente métodos para buscar produtos por nome, calcular 
+// o valor total em estoque e atualizar a quantidade de um produto.
+class Produtos{
+    constructor(nome, preco, quantidadeEstoque){
+        this.nome = nome
+        this.preco = preco
+        this.quantidadeEstoque = quantidadeEstoque
+    }
+}
+
