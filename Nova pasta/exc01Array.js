6a18c8132694c02b255d7cae7f726cd4532b9907
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-05-28 21:20:46 -0300

    Update exc01Array.js

--- Nova pasta/exc01Array.js
+++ Nova pasta/exc01Array.js
@@ -7,12 +7,12 @@
 // }
 let nomes = ['bruna', 'ludmilla', 'maria', 'joão', 'bianca']
 
-let contadorDeInicial = array.reduce((nomes, nomeAtual)=>{
-    let primeiraLetra = nomeAtual[0]
-    if(nomeAtual[primeiraLetra]){
-        nomeAtual[primeiraLetra]++
-    }else {
-        nomeAtual[primeiraLetra] = 1
-    }
-}, {})
-console.log(contadorDeInicial)
+// let contadorDeInicial = array.reduce((nomes, nomeAtual)=>{
+//     let primeiraLetra = nomeAtual[0]
+//     if(nomeAtual[primeiraLetra]){
+//         nomeAtual[primeiraLetra]++
+//     }else {
+//         nomeAtual[primeiraLetra] = 1
+//     }
+// }, {})
+// console.log(contadorDeInicial)
