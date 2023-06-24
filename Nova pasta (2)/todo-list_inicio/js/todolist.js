1831618961f9b83543b69af4ce39bfe671019974
Bruna Fernandes Cardoso <126504526+BrunaCardoso7@users.noreply.github.com>
2023-06-24 13:38:34 -0300

    Update todolist.js

--- Nova pasta (2)/todo-list_inicio/js/todolist.js
+++ Nova pasta (2)/todo-list_inicio/js/todolist.js
@@ -5,7 +5,7 @@
     const itemInput =  document.querySelector('#item-input')
     const toDoAddForm = document.querySelector('#todo-add')
     const ul_toDo = document.querySelector("#todo-list")
-    // const lis = ul_toDo.getElementsByTagName('li')
+    const lis = ul_toDo.getElementsByTagName('li')
 
     const arrTask = [
         {
@@ -15,12 +15,12 @@
         }
     ]
 
-    function addEventLi(li){
-        li.addEventListener('click', function(){
-            console.log(li)
+    // function addEventLi(li){
+    //     li.addEventListener('click', function(){
+    //         console.log(li)
 
-        })
-    }
+    //     })
+    // }
     
 
     function generatorAddTask(obj){
@@ -31,6 +31,9 @@
         button.className = 'button-check'
         const i_check = document.createElement('i')
         i_check.className = 'fas fa-check displayNone'
+        i_check.setAttribute('data-action', 'checked')
+
+        button.setAttribute('data-action', 'checkButton')
         button.appendChild(i_check)
 
         const p = document.createElement('p')
@@ -38,51 +41,50 @@
         p.textContent = obj.name
 
         const i_edit = document.createElement('i')
+        i_edit.setAttribute('data-action', 'editInput')
         i_edit.className = 'fas fa-edit'
 
-        i_edit.addEventListener('click', function (e){
+        
+        const conteinerDiv = document.createElement('div')
+        conteinerDiv.className = 'editContainer'
+     
 
-            const conteinerDiv = document.createElement('div')
-            conteinerDiv.className = 'editContainer'
-            conteinerDiv.style.display = 'flex'
+        const inputEdit = document.createElement('input')
+        inputEdit.setAttribute('type', 'text')
+        inputEdit.className = 'editInput'
+        conteinerDiv.appendChild(inputEdit)
+        
+        const btn_edit =  document.createElement('button')
+        btn_edit.className = 'editButton'
+        btn_edit.textContent = 'Edit'
+        btn_edit.setAttribute('data-action', 'tbn_edited')
+        conteinerDiv.appendChild(btn_edit)
+        
+        const btn_cancel =  document.createElement('button')
+        btn_cancel.className = 'cancelButton'
+        btn_cancel.setAttribute('data-action', 'tbn_canceled')
+        btn_cancel.textContent = 'Cancel'
 
-            const inputEdit = document.createElement('input')
-            inputEdit.setAttribute('type', 'text')
-            inputEdit.className = 'editInput'
-            conteinerDiv.appendChild(inputEdit)
-            
-            const btn_edit =  document.createElement('button')
-            btn_edit.className = 'editButton'
-            btn_edit.textContent = 'Edit'
-            conteinerDiv.appendChild(btn_edit)
-            
-            const btn_cancel =  document.createElement('button')
-            btn_cancel.className = 'cancelButton'
-            btn_cancel.textContent = 'Cancel'
-            btn_cancel.addEventListener('click', function (){
-                conteinerDiv.style.display = 'none'
-            })
-            conteinerDiv.appendChild(btn_cancel)
-            
+    
+        conteinerDiv.appendChild(btn_cancel)
+        
 
 
-            li.appendChild(conteinerDiv)
-
-        })
-
+        li.appendChild(conteinerDiv)
 
 
 
         
         const i_remove = document.createElement('i')
         i_remove.className = 'fas fa-trash-alt'
+        i_remove.setAttribute('data-action', 'removeInput')
 
         li.appendChild(button)
         li.appendChild(p)
         li.appendChild(i_edit)
         li.appendChild(i_remove)
 
-        addEventLi(li)
+        // addEventLi(li)
 
         return li
     }
@@ -104,6 +106,53 @@
         })
     }
 
+    function clickedUl(evt){
+        console.log(evt.target)
+        let dataAction = evt.target.getAttribute('data-action')
+        console.log(dataAction)
+
+        if(!dataAction) return
+
+        let currentLi =  evt.target
+        while(currentLi.nodeName !== 'LI'){
+            currentLi = currentLi.parentElement
+        }
+        console.log(currentLi)
+
+        const currentLiIndex = [...lis].indexOf(currentLi)
+        console.log(currentLiIndex)
+        
+        let actions = {
+            editInput: function(){
+                const divConteiner = currentLi.querySelector('.editContainer')
+                divConteiner.style.display = 'flex'
+                // renderTask()
+            },
+            removeInput: function(){
+                arrTask.splice(currentLiIndex, 1)
+                renderTask()
+            },
+            tbn_canceled: function(){
+                const divConteiner = currentLi.querySelector('.editContainer')
+                divConteiner.style.display = 'none'
+            },
+            tbn_edited: function(){
+                const valueInput = currentLi.querySelector('.editInput').value
+                arrTask[currentLiIndex].name = valueInput
+                renderTask()
+            },
+            checkButton: function(){
+                const checked = currentLi.querySelector('.fa-check')
+                checked.style.display = 'block'
+            }
+
+        }
+
+        if(actions[dataAction]){
+            actions[dataAction]()
+        }
+    }
+
     toDoAddForm.addEventListener('submit', function(e){
         e.preventDefault()//para não enviar o formulário
         addTask(itemInput.value)
@@ -111,6 +160,8 @@
         itemInput.value = ''
         itemInput.focus
     });
+
+    ul_toDo.addEventListener('click', clickedUl)
 
     renderTask()
 
